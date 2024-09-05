4ff1753d736b70d5d4134a55f593c5b42656453e
JoaoVrf13 <joaovictorrfcontato@gmail.com>
2024-09-05 18:57:25 -0300

    Sistema de aumento salarial, melhor estruturado

---
+++ Employees.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace list_employees {
+    internal class Employe {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+
+        public Employe(int id, string name, double salary) {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void Increase(double percentage) {
+
+            Salary += Salary * percentage / 100.0;
+
+        }
+
+
+
+        public override string ToString () {
+            return $"ID: {Id}, Name: {Name}, Salary: {Salary:F2}";
+             
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+
+namespace list_employees {
+    class Program {
+        static void Main(string[] args) {
+
+
+            List<Employe> employees = new List<Employe>();
+
+
+            Console.Write("How many employees will be registrered? ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++) {
+                Console.WriteLine($"Employees #{i + 1}:");
+                Console.Write("Id: ");
+                int Id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string Name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double Salary = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+                employees.Add(new Employe(Id, Name, Salary));
+            }
+
+            Console.Write("Enter the employe id that will have salary increase : ");
+            int increaseId = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the percentage: ");
+            double percentage = double.Parse(Console.ReadLine());
+
+            Employe emp = employees.Find(e => e.Id == increaseId);
+            if(emp != null) {
+                emp.Increase(percentage);
+            }
+            else {
+                Console.WriteLine("Employee with the give ID not found.");
+            }
+
+            Console.WriteLine("\n Updated employee details: ");
+            foreach(Employe e in employees) {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
+
